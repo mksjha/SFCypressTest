33bdc8391570c1cd87ae033b37e7b551d1cddf0a
Mukesh Kumar Jha <mukeshkumar.jha@anz.com>
2020-03-05 14:39:07 +1100

    cypress and shadow dom

---
+++ cypress/integration/SF/POC.js
@@ -0,0 +1,26 @@
+describe('Login Page', function() {
+  it('Visit the salesforce', function() {
+
+    cy.request('https://mksjha-dev-ed.my.salesforce.com/?un=&pw=')
+     cy.visit('https://mksjha-dev-ed.lightning.force.com/lightning/o/Account/list?filterName=Recent')
+     cy.wait(10000)
+     .get('.forceOutputLookup').contains('Burlington Textiles Corp of America').click()
+    
+     //unable to fins shadow dom element through cypress
+     //.get('input[name="Name"]').type('12345')
+     
+     //not sure why jquery not working. jquery is able to find element through console
+
+    //  const $li = Cypress.$('input[name="Name"]')
+    //  cy.wrap($li).type('1234')
+    
+    //with shadow dom plug in shadow type method is working but type is not working
+    //.shadowGet('c-first').shadowFind('lightning-record-form').shadowFind('lightning-record-edit-form').shadowFind('lightning-input-field').shadowFind('lightning-input').shadowFind('input[name="Name"]').shadowType('mukesh')
+    .shadowGet('c-first').shadowFind('lightning-record-form').shadowFind('lightning-record-edit-form').shadowFind('lightning-input-field').shadowFind('lightning-input').shadowFind('input[name="Name"]').type('mukesh')
+      
+  })
+
+})
+
+
+
